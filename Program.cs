332360a9d59f7c7e64a64a7a951e75eeae3b97a8
Nikhil Anand <112142828+Nikhilmvk@users.noreply.github.com>
2026-02-07 09:55:31 +0530

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyOrigin()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod());
+});
+
+var app = builder.Build();
+
+app.UseCors("AllowAll");
+app.UseStaticFiles();
+app.MapControllers();
+
+app.Run();
